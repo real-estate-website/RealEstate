fc2977163fb8f676654d3a77bc64eaa2211c9c8b
Ricardo <abrahamricardogn224@gmail.com>
2022-04-27 23:09:33 +0200

    git suck

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -18,7 +18,7 @@
                 </div>
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-nav-link :href="route('createannonce')" :active="request()->routeIs('createannonce')">
-                        {{ __('Créer votre Annonce') }}
+                        {{ __('CrÃ©er votre Annonce') }}
                     </x-nav-link>
                 </div>
             </div>
